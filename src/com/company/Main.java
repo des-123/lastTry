03c98f0ce696cca75b652af75e841e836b356858
desmond <102528054+des-123@users.noreply.github.com>
2022-04-11 23:47:51 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,16 +12,31 @@ public class Main {
         String fillepath2 = "cuttree.wav"; //crab
         String fillepath3 = "hitmonster.wav"; //Octopus
         String fillepath4 = "powerup.wav"; //Meteor
-        String fillepath5 = "recievedamage.wav"; //shooter
+        String fillepath5 = "recievedamage.wav"; //shooter/player
 
 
-
+// calling the song for each diff object meteor,alians or shooter
 
        musicthings musicPlayer1 = new musicthings();
        musicPlayer1.playMusic(fillepath2);
 
         musicthings musicPlayer2 = new musicthings();
         musicPlayer2.playMusic(fillepath2);
+  
+        musicthings musicPlayer3 = new musicthings();
+        musicPlayer2.playMusic(fillepath2);
+
+        musicthings musicPlayer4 = new musicthings();
+        musicPlayer2.playMusic(fillepath3);
+
+         musicthings musicPlayer5 = new musicthings();
+        musicPlayer2.playMusic(fillepath4);
+
+        musicthings musicPlayer6 = new musicthings();
+        musicPlayer2.playMusic(fillepath5);
+	    
+	    
+	    
 
     }
 }
